2d8e7943d967631ebf26fa59f03094b86e2b2d72
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2019-01-20 16:44:25 -0800

    Delete beer_ids.js

--- beerselector_api/server/controllers/beer_ids.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require ('mongoose');
-var Beer_ids = mongoose.model('Beer_ids');
-
-module.exports = {
-    favorite: function (req, res) {
-       req.session.beer_ids = beer_ids;
-    }  
-}
