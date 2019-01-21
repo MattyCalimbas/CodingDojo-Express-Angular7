8b6406a3763df3aa31d7dfcd678a0da14ad2b3b7
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2019-01-20 16:44:41 -0800

    Delete mongoose.js

--- beerselector_api/server/config/mongoose.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-var fs = require('fs');
-var models_path = path.join(__dirname, './../models');
-fs.readdirSync(models_path).forEach(function(file) {
-    if(file.indexOf('.js') >= 0) {
-        require(models_path + '/' + file);
-    }
-})
